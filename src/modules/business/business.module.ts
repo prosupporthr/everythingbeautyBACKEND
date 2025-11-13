c254d40c5e5b7462a99f8be550b62da397d5b53a
Neon <danielemmanuel257@gmail.com>
2025-11-13 13:42:54 +0100

    feat(business): add Service schema to business module imports

--- src/modules/business/business.module.ts
+++ src/modules/business/business.module.ts
@@ -6,12 +6,14 @@ import { Business, BusinessSchema } from '@schemas/Business.schema';
 import { User, UserSchema } from '@/schemas/User.schema';
 import { JwtService } from '@nestjs/jwt';
 import { UploadService } from '../upload/upload.service';
+import { Service, ServiceSchema } from '@/schemas/Service.Schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: Business.name, schema: BusinessSchema },
       { name: User.name, schema: UserSchema },
+      { name: Service.name, schema: ServiceSchema },
     ]),
   ],
   controllers: [BusinessController],
